a72666be4f3067b017a25e4beca7b0309f5abb1a
Jovan Goh <54385240+jovanimal@users.noreply.github.com>
2020-02-18 07:01:11 +0800

    add build files

---
+++ src/components/build/precache-manifest.75d2feda85c22facec2284b2c7fbbad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d42fdc501276d1c70406a0d408996d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e7a8c05ec697c30d468",
+    "url": "/static/css/2.32793d14.chunk.css"
+  },
+  {
+    "revision": "f931458b8b57a11004ce",
+    "url": "/static/css/main.1fc1d72f.chunk.css"
+  },
+  {
+    "revision": "5e7a8c05ec697c30d468",
+    "url": "/static/js/2.4e8163a6.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.4e8163a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f931458b8b57a11004ce",
+    "url": "/static/js/main.fe8f6986.chunk.js"
+  },
+  {
+    "revision": "7050f19bc0c4efa02583",
+    "url": "/static/js/runtime-main.b284ec8a.js"
+  }
+]);
