1e6845bf6f0975ee235d752fced3ee870cc29bca
Jovan Goh <54385240+jovanimal@users.noreply.github.com>
2020-02-17 18:39:30 +0800

    upload nextagram files

---
+++ src/pages/MyProfilePage.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, NavLink as Link } from "react-router-dom";
+import { Container, Col, Row, Button } from "reactstrap";
+import Image from "react-graceful-image";
+import Loader from "../components/Loader";
+import axios from "axios";
+
+const MyProfilePage = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [images, setImages] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("userInfo");
+    if (loggedInUser) {
+      setCurrentUser(JSON.parse(loggedInUser));
+    } else {
+      history.push("/");
+    }
+
+    axios({
+      method: "GET",
+      url: "https://insta.nextacademy.com/api/v1/images/me",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("authToken")}`
+      }
+    }).then(response => {
+      setImages(response.data);
+    });
+  }, []);
+
+  if (!currentUser) {
+    return <Loader primaryColor="yellow" secondaryColor="red" size="200px" />;
+  }
+
+  console.log(currentUser);
+
+  return (
+    <div>
+      <Container className="mt-5" fluid>
+        <Row>
+          <Col md={6}>
+            <Image
+              src={currentUser.profile_picture}
+              style={{ width: "auto", borderRadius: "50%", height: "200px" }}
+              alt="me"
+            />
+          </Col>
+          <Col md={6}>
+            <h1>{currentUser.username}</h1>
+            <Button
+              className="mt-4"
+              color="secondary"
+              tag={Link}
+              to="/image/upload"
+            >
+              Upload New Image
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <hr />
+        {images.length > 0 ? (
+          <div
+            // className="w-100 mt-5"
+            style={{ columnCount: 4, columnGap: "1em" }}
+          >
+            {images.map((image, index) => {
+              return (
+                <Image
+                  key={index}
+                  src={image}
+                  style={{ width: "100%", margin: "0 0 1em", display: "block" }}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <h1 className="mt-5">No Images to display! Make a post!</h1>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default MyProfilePage;
---
+++ src/pages/UploadPage.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { Container, FormGroup, CustomInput, Form, Button } from "reactstrap";
+import axios from "axios";
+import { NavLink as Link } from "react-router-dom";
+
+const UploadPage = () => {
+  const [previewImage, setPreviewImage] = useState(null);
+  const [uploadImage, setUploadImage] = useState(null);
+  const [responseMessage, setResponseMessage] = useState(null);
+
+  const handleUpload = e => {
+    e.preventDefault();
+
+    // axios call to upload to the server
+    let formData = new FormData();
+
+    formData.append("image", uploadImage);
+
+    let jwt = localStorage.getItem("authToken");
+
+    axios
+      .post("https://insta.nextacademy.com/api/v1/images/", formData, {
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        }
+      })
+      .then(result => {
+        if (result.data.success) {
+          setResponseMessage("Image uploaded successfully!");
+        } else {
+          setResponseMessage("Error uploading Image.");
+        }
+      })
+      .catch(error => {
+        console.log(error.response);
+        setResponseMessage("Major error, contact admin!");
+      });
+  };
+
+  const handleImage = e => {
+    // Handle the preview and file to upload
+    let imageFile = e.target.files[0];
+
+    let newImage = URL.createObjectURL(imageFile);
+
+    setPreviewImage(newImage);
+    setUploadImage(imageFile);
+  };
+
+  return (
+    <Container className="pt-5">
+      <h6>Upload New Image</h6>
+      <div
+        className="border border-light rounded mx-auto d-block mt-4"
+        style={{ height: "500px", width: "500px", position: "relative" }}
+      >
+        {previewImage ? (
+          responseMessage ? (
+            <>
+              <h2
+                style={{
+                  position: "absolute",
+                  left: "50%",
+                  top: "50%",
+                  transform: "translate(-50%, -50%)"
+                }}
+              >
+                {responseMessage}
+              </h2>
+              <Button
+                outline
+                color="success"
+                tag={Link}
+                to="/profile"
+                style={{
+                  position: "absolute",
+                  left: "50%",
+                  top: "65%",
+                  transform: "translate(-50%, -50%)"
+                }}
+              >
+                My profile
+              </Button>
+            </>
+          ) : (
+            <img
+              className="w-75"
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                height: "400px",
+                width: "auto",
+                transform: "translate(-50%, -50%)"
+              }}
+              src={previewImage}
+              alt="preview"
+            />
+          )
+        ) : (
+          <h2
+            style={{
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)"
+            }}
+          >
+            Choose image to preview
+          </h2>
+        )}
+      </div>
+      <Form onSubmit={handleUpload}>
+        <FormGroup className="w-50 mx-auto d-block">
+          <CustomInput
+            type="file"
+            id="exampleCustomFileBrowser"
+            name="customFile"
+            className="mt-4"
+            onChange={handleImage}
+          />
+        </FormGroup>
+        <Button outline color="primary">
+          Upload
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+// URL.createObjectURL(e.target.files[0])
+// https://insta.nextacademy.com/api/v1/images/
+
+export default UploadPage;
